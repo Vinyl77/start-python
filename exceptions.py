aa688718c7eb575d884071c651ac9ad8d1bfaa2b
Richard Yvarra <richard.yvarra@ibm.com>
2021-12-10 15:50:34 -0500

    add exceptions file

---
+++ exceptions.py
@@ -0,0 +1,18 @@
+student = {
+    "name": "Mark",
+    "student_id": 15163,
+    "feedback": None
+}
+
+student["last_name"] = "Kowalski"
+
+try:
+    last_name = student["last_name"]
+    numbered_last_name = 3 + last_name
+except KeyError:
+    print("Error finding last_name")
+
+except TypeError:
+    print("I can't add these two together!")
+
+print("This code executes!")
