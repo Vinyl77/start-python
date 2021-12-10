6da127db090a4b942a42ada449b8f5c1876ad652
Richard Yvarra <richard.yvarra@ibm.com>
2021-12-10 16:58:15 -0500

    changes to function.py

--- exceptions.py
+++ exceptions.py
@@ -12,7 +12,8 @@ try:
 except KeyError:
     print("Error finding last_name")
 
-except TypeError:
+except TypeError as error:
     print("I can't add these two together!")
+    print(error)
 
 print("This code executes!")
