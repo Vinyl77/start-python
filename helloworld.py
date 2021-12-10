a605f8aa67bdf967cad795114b015b648311897d
Richard Yvarra <richard.yvarra@ibm.com>
2021-12-10 14:15:00 -0500

    more test code

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,13 @@
 print("Hello World!")
 
- 
+
+student_names = ["Neha", "Mike", "Patrick"]
+
+for name in student_names:
+    print("Studen name is {0}".format(name)) 
+
+x = 0
+for index in range(10):
+    x += 10
+    print("The value of X is {0}".format(x))
+    
