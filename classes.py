dd909651a93c222d0494ed3f950da297a08675d8
Richard Yvarra <richard.yvarra@ibm.com>
2021-12-13 13:59:15 -0500

    finished the course

--- classes.py
+++ classes.py
@@ -1,37 +1,37 @@
-students = []
+# students = []
 
 
-class Student:
+# class Student:
 
-	school_name = "Springfield Elementary"
+# 	school_name = "Springfield Elementary"
 
-	def __init__(self, name, student_id=332):
-		self.name = name
-		self.student_id = student_id
-		students.append(self)
+# 	def __init__(self, name, student_id=332):
+# 		self.name = name
+# 		self.student_id = student_id
+# 		students.append(self)
 
-	def __str__(self):
-		return "Student " + self.name
+# 	def __str__(self):
+# 		return "Student " + self.name
 
-	def get_name_capitalize(self):
-		return self.name.capitalize()
+# 	def get_name_capitalize(self):
+# 		return self.name.capitalize()
 
-	def get_school_name(self):
-		return self.school_name
-
-
-class HighSchoolStudent(Student):
-    school_name = "Springfield High School"
-
-    def get_school_name(self):
-        return "This is a High School student"
-
-    def get_name_capitalize(self):
-        orginal_value = super().get_name_capitalize()
-        return orginal_value + "-HS"
+# 	def get_school_name(self):
+# 		return self.school_name
 
 
+# class HighSchoolStudent(Student):
+#     school_name = "Springfield High School"
+
+#     def get_school_name(self):
+#         return "This is a High School student"
+
+#     def get_name_capitalize(self):
+#         orginal_value = super().get_name_capitalize()
+#         return orginal_value + "-HS"
 
 
-james = HighSchoolStudent("james")
-print(james.get_name_capitalize())
+
+
+# james = HighSchoolStudent("james")
+# print(james.get_name_capitalize())
